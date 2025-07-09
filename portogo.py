ea1cf693cce9c0d874d3b3d2fe4e1bc2ba66fc77
sonntagUX <dedesonntag@gmail.com>
2025-07-09 08:19:33 -0700

    Update portogo.py

--- portogo.py
+++ portogo.py
@@ -2,6 +2,21 @@ import streamlit as st
 import random
 import time
 
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
 st.title("Simple chat")
 
 # Initialize chat history
@@ -15,8 +30,14 @@ for message in st.session_state.messages:
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
