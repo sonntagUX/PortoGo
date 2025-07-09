a242eedba9c109b4c37536c1a94c708862a178db
sonntagUX <dedesonntag@gmail.com>
2025-07-09 08:17:42 -0700

    Update portogo.py

--- portogo.py
+++ portogo.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import random
 import time
-import requests
 
-st.header("PortoGo")
-st.subheader(" (Powered by Hugging Face)")
-st.write("Ask me anything about relocating from the U.S. to Portugal!")
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
+
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -23,5 +20,3 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-
